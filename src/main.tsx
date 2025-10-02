da019cfecb8987670c1597d08845859a3aca6e38
Luka <luka.akhalkatsi.1@btu.edu.ge>
2025-10-01 23:49:55 -0700

    SearchProvider wrap

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
+import { SearchProvider } from "./context/SearchContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SearchProvider>
+        <App />
+      </SearchProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
